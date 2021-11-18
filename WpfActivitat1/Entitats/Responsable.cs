54799e5079745f994d6c2bc912834faf375b63b1
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2021-11-18 01:34:37 +0100

    Objecte responsable i Tasca

---
+++ WpfActivitat1/Entitats/Responsable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfActivitat1.Entitats
+{
+    class Responsable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        
+    }
+}
---
+++ WpfActivitat1/Entitats/Tasca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfActivitat1.Entitats
+{
+    class Tasca
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Descripcio { get; set; }
+        public string responsable { get; set; }
+        public enum Colors{ red , yellow, green }
+        public DateTime data_final { get; set; }
+        public DateTime data_inici { get; set; }
+
+
+    }
+}
