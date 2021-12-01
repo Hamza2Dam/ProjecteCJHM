e6ddc7bcd858ddb9d09c2fc9fd4e4be707024f56
Muhammad Hamza <mhamza.dam@institutcampalans.net>
2021-12-01 16:41:20 +0100

    0

--- SQLiteExampleV2/Persistence/DbContext.cs
+++ SQLiteExampleV2/Persistence/DbContext.cs
@@ -38,46 +38,6 @@ namespace SQLiteExampleV2.Persistence
                             command.ExecuteNonQuery();
                         }
                     }
-
-                    // Insert a Users
-                    for (var i = 1; i <= 15; i++)
-                    {
-                        var query = "INSERT INTO Users (name, lastname, birthday) VALUES (?, ?, ?)";
-
-                        using (var command = new SQLiteCommand(query, ctx))
-                        {
-                  
-                            command.Parameters.Add(new SQLiteParameter("name", ""));
-                            command.Parameters.Add(new SQLiteParameter("lastname", "" ));
-
-                            var rnd = new Random();
-                            command.Parameters.Add(new SQLiteParameter("birthday", DateTime.Today.AddYears(-rnd.Next(1, 50))));
-
-                            command.ExecuteNonQuery();
-                        }
-                    }
-
-                    // Insert a Tasca
-                    for (var i = 1; i <= 10; i++)
-                    {
-                        var query = "INSERT INTO Tasca (Nom, Descripcio, Responsable, Colors, Data_Inici, Data_Final, Estat) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-                        using (var command = new SQLiteCommand(query, ctx))
-                        {
-
-                            command.Parameters.Add(new SQLiteParameter("Nom", "" ));
-                            command.Parameters.Add(new SQLiteParameter("Descripcio", ""));
-                            command.Parameters.Add(new SQLiteParameter("Responsable", ""));
-                            command.Parameters.Add(new SQLiteParameter("Colors", " " ));
-
-                            var rnd = new Random();
-                            command.Parameters.Add(new SQLiteParameter("Data_Inici", DateTime.Today.AddYears(-rnd.Next(1, 50))));
-                            command.Parameters.Add(new SQLiteParameter("Data_Final", DateTime.Today.AddYears(-rnd.Next(1, 50))));
-                            command.Parameters.Add(new SQLiteParameter("Estat", "TODO"));
-
-                            command.ExecuteNonQuery();
-                        }
-                    }
                 }
             }
         }
